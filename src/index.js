66ef165030d42c747a937f3777190bc318da32f1
Mihnea Lazar <mihnea.lazar@accesa.eu>
2020-10-30 09:59:59 +0200

    [NoRef][Fundamentals] Passing lists

--- src/index.js
+++ src/index.js
@@ -15,47 +15,47 @@ import './index.css'
 // close every element
 // formatting
 
-const firstBook = {
+const books = [{
+  id: 1,
   author: 'Iain Banks',
   title: "The wasp factory",
   img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1434940562l/567678._SY475_.jpg"
-};
-
-const secondBook = {
+}, {
+  id: 2,
   author: 'Herman Hesse',
   title: 'Siddhartha',
   img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1428715580l/52036.jpg'
-}
-
-const thirdBook = {
+}, {
+  id: 3,
   author: 'Albert Camus',
   title: 'The Stranger',
   img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1590930002l/49552._SY475_.jpg'
-}
+}, {
+  id: 4,
+  author: 'Timothy F.Geithner',
+  title: 'Stress Test: Reflections on Financial Crises',
+  img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1418769295l/22889809.jpg'
+}];
 
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
-      <Book img={thirdBook.img} title={thirdBook.title} author={thirdBook.author}>
-        <p>Lorem ipsum etc etc </p>
-      </Book>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return (
+          <Book key={book.id} book={book} />
+        )
+      })}
     </section>
   );
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return <article className='book'>
     <Image img={img} />
     <Title title={title} />
     <Author author={author} />
-    {children}
   </article>
 }
 const Image = (props) => {
