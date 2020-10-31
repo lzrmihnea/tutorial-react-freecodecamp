3666f7ffc1fb74549d714e729c1d545ba1df2b14
Mihnea Lazar <mihnea.lazar@accesa.eu>
2020-10-31 09:46:30 +0200

    [NoRef][Advanced] Using state on an object

--- tutorial-react-02-advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ tutorial-react-02-advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random'
+  });
+  const [name,setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('random person')
+  const changeMessage = () => {
+    // setPerson({...person, message: 'Hello world'}); // overrides only the message field of the person
+    setMessage('Hello world');
+  }
+  return (<>
+  <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
+    <button className="btn" onClick={changeMessage}>change message</button>
+  </>);
 };
 
 export default UseStateObject;
