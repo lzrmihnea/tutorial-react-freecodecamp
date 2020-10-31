62efd4d96eee34e92ba43ba2b0a6978c2e87630a
Mihnea Lazar <mihnea.lazar@accesa.eu>
2020-10-31 11:23:22 +0200

    [NoRef][Advanced] Using state on counters with functions and timeout

--- tutorial-react-02-advanced/src/tutorial/1-useState/setup/5-useState-counter.js
+++ tutorial-react-02-advanced/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -4,12 +4,17 @@ const UseStateCounter = () => {
   const [value, setValue] = useState(0);
 
   const modifyBy = (newValue) => {
-    if(newValue===0) {
+    if (newValue === 0) {
       setValue(0);
     } else {
       setValue(value + newValue);
     }
   };
+
+  const complexIncrease = (newValue) => {
+    setTimeout(() => { setValue(value + newValue) }, 2000);
+  };
+
   return (<>
     <section style={{ margin: '4rem 0' }}>
       <h2>regular counter</h2>
@@ -17,6 +22,7 @@ const UseStateCounter = () => {
       <button className="btn" onClick={() => modifyBy(1)}>+</button>
       <button className="btn" onClick={() => modifyBy(0)}>0</button>
       <button className="btn" onClick={() => modifyBy(-1)}>-</button>
+      <button className="btn" onClick={() => complexIncrease(1)}>increase later</button>
     </section>
   </>);
 };
