6c82912a69e3c366107161d422bffe31418bdd9b
Mihnea Lazar <mihnea.lazar@accesa.eu>
2020-10-31 09:53:15 +0200

    [NoRef][Advanced] Counter, value increment, decrement, reset

--- tutorial-react-02-advanced/src/tutorial/1-useState/setup/5-useState-counter.js
+++ tutorial-react-02-advanced/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const modifyBy = (newValue) => {
+    if(newValue===0) {
+      setValue(0);
+    } else {
+      setValue(value + newValue);
+    }
+  };
+  return (<>
+    <section style={{ margin: '4rem 0' }}>
+      <h2>regular counter</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => modifyBy(1)}>+</button>
+      <button className="btn" onClick={() => modifyBy(0)}>0</button>
+      <button className="btn" onClick={() => modifyBy(-1)}>-</button>
+    </section>
+  </>);
 };
 
 export default UseStateCounter;
