db20600a1e2f02c5f85a89f288a12d2fb440a6b4
Mihnea Lazar <mihnea.lazar@accesa.eu>
2020-11-02 11:43:12 +0200

    [NoRef][Advanced] UseEffect with dependencies

--- tutorial-react-02-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ tutorial-react-02-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+
   useEffect(() => {
     console.log('call useEffect');
     document.title = `New Messages(${value})`;
-  });
+  }, [value]); //the array has dependencies
+
+  useEffect(() => {
+    console.log('this will only run on the initial render, '
+      + 'because of the empty dependency array');
+  }, []);
+
   console.log('render component');
 
   return (<>
