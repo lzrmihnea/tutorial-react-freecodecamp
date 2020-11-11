b32fba6c9c43e71293bfa509f39a2844cc2abebc
Mihnea Lazar <mihnea.lazar@accesa.eu>
2020-11-11 12:21:52 +0200

    [NoRef][Advanced] PropTypes, default props, default types, fallbacks

--- tutorial-react-02-advanced/src/tutorial/10-prop-types/setup/Product.js
+++ tutorial-react-02-advanced/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  const imgUrl = image && image.url
+  return (
+    <article className='product'>
+      <h4>single product</h4>
+      <img src={imgUrl || defaultImage} alt={name || 'default name'} /> 
+    {/* adding such operators for default behavior is possible, but more messy than using defaultProps */}
+      <h4>{name}</h4>
+      <p>${price || 3.99}</p>
+    </article>);
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+};
+
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   image: { url: defaultImage },
+// };
 
 export default Product;
